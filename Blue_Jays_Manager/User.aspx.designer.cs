1f53be6ecb30eae42e6f04cdb1592a714c092077
Vinood Persad <vinood.persad@gmail.com>
2016-08-04 12:12:29 -0400

    All images added and available

--- Blue_Jays_Manager/User.aspx.designer.cs
+++ Blue_Jays_Manager/User.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace Blue_Jays_Manager {
         protected global::System.Web.UI.WebControls.TextBox newPass;
         
         /// <summary>
+        /// PasswordRegularExpressionValidator control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator PasswordRegularExpressionValidator;
+        
+        /// <summary>
         /// confirmPass control.
         /// </summary>
         /// <remarks>
